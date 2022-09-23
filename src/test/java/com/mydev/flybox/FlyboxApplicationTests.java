a53b4991cbcd37235a268956a5e1600c4e726989
KyleRobison15 <kylerobison15@gmail.com>
2022-09-23 11:32:48 -0600

    initial commit

---
+++ src/test/java/com/mydev/flybox/FlyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydev.flybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
